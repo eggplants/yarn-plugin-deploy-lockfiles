04ec842690ff0ac48f64ebe1210c213f9f7ad4bc
Svetlozar <ext-svetlozar@getitdone.co>
2023-02-09 15:24:54 +0200

    Generate yarn.deploy.lock per workspace directory.

--- sources/index.ts
+++ sources/index.ts
@@ -1,42 +1,51 @@
-import { Cache, Project, structUtils, ThrowReport } from '@yarnpkg/core';
+import { Cache, Project, structUtils, ThrowReport, Workspace } from '@yarnpkg/core';
 import { ppath, xfs } from '@yarnpkg/fslib';
 import * as globrex from 'globrex';
 
 import type { Filename, PortablePath } from '@yarnpkg/fslib';
-import type { Configuration, Hooks, Report, Workspace } from '@yarnpkg/core';
+import type { Configuration, Hooks, Report } from '@yarnpkg/core';
 
 const afterAllInstalled: Hooks['afterAllInstalled'] = async (project, options) => {
-  await options.report.startTimerPromise('Entrypoint lockfiles', () =>
+  const ws = project.workspacesByCwd.get(project.configuration.startingCwd);
+  await options.report.startTimerPromise(`Deployment lockfiles (${ws.relativeCwd})`, () =>
     generateLockfiles(project.configuration, project, options.report)
   );
 };
 
 async function generateLockfiles(configuration: Configuration, project: Project, report: Report) {
+  const startingWs = project.workspacesByCwd.get(project.configuration.startingCwd);
   const cache = await Cache.find(configuration, { immutable: true });
-  const entrypoints = await getEntrypoints(project);
+  const deployments = startingWs.relativeCwd === '.' ? await getDeployments(project) : [startingWs];
+  const workspaceReferences = new Set(deployments.map(d => 'workspace:' + d.relativeCwd));
 
   await Promise.all(
-    entrypoints.map(async workspace => {
-      const lockfileName = `yarn.${structUtils.slugifyIdent(workspace.locator)}.lock` as Filename;
-      const lockfilePath = ppath.join(project.cwd, lockfileName);
+    deployments.map(async workspace => {
+      const lockfileName = `yarn.deploy.lock` as Filename;
+      const lockfilePath = ppath.join(workspace.cwd, lockfileName);
 
-      const lockfile = await generateLockfile(configuration, workspace.cwd, cache);
+      const lockfile = await generateLockfile(configuration, workspace.cwd, cache, workspaceReferences);
       await xfs.writeFilePromise(lockfilePath, lockfile);
-      report.reportInfo(null, `${structUtils.stringifyIdent(workspace.locator)} => ${lockfileName}`);
+      report.reportInfo(null, `${structUtils.stringifyIdent(workspace.locator)} => ${lockfilePath}`);
     })
   );
 }
 
-async function getEntrypoints(project: Project) {
+async function getDeployments(project: Project) {
   const rootWorkspace = project.workspacesByCwd.get(project.cwd);
-  const entrypoints: string[] = rootWorkspace.manifest.raw.workspaces?.entrypoints || [];
-  const entrypointPatterns = entrypoints.map(glob => globrex(glob, { globstar: true, extended: true }).regex);
+  const deploymentPatterns = rootWorkspace.manifest.workspaceDefinitions.map(
+    glob => globrex(glob.pattern, { globstar: true, extended: true }).regex
+  );
 
-  const isEntrypoint = (workspace: Workspace) => entrypointPatterns.some(regexp => regexp.test(workspace.relativeCwd));
+  const isEntrypoint = (workspace: Workspace) => deploymentPatterns.some(regexp => regexp.test(workspace.relativeCwd));
   return project.workspaces.filter(isEntrypoint);
 }
 
-async function generateLockfile(configuration: Configuration, workspaceCwd: PortablePath, cache: Cache) {
+async function generateLockfile(
+  configuration: Configuration,
+  workspaceCwd: PortablePath,
+  cache: Cache,
+  workspaceReferences: Set<string>
+) {
   // Always create a new instance of Project to avoid interference of its internal state
   const { project, workspace } = await Project.find(configuration, workspaceCwd);
 
@@ -44,6 +53,25 @@ async function generateLockfile(configuration: Configuration, workspaceCwd: Port
   project.workspaces = [workspace];
 
   await project.resolveEverything({ cache, report: new ThrowReport() });
+
+  const workSpaceRef = 'workspace:' + workspace.relativeCwd;
+  for (const pkg of project.originalPackages.values()) {
+    if (pkg.reference == workSpaceRef) {
+      pkg.reference = 'workspace:.';
+      continue;
+    }
+    if (workspaceReferences.has(pkg.reference)) {
+      pkg.reference;
+    }
+  }
+
+  for (const descriptor of project.storedDescriptors.values()) {
+    if (descriptor.range == workSpaceRef) {
+      descriptor.range = 'workspace:.';
+      continue;
+    }
+  }
+
   return project.generateLockfile();
 }
 
